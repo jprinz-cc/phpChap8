706575a226a2b4735c6c6bab97563fefa056f13c
jprinz <jprinz@conestogac.on.ca>
2017-06-21 08:29:30 -0400

    Init commit; Upto page 219;

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/* This is the home page for this site.
+It uses templates to create the layout.*/
+
+define('TITLE', 'Raise High the Roof Beam!');
+
+include('templates/header.html');
+?>
+    <h2>Welcome to a J.D. Salinger Fan Club</h2>
+    <p>Lucas ipsum dolor sit amet motti malastare c-3p0 ti hypori skywalker ooryl sio noa iego. Coruscant hssis calamari nagai. Boltrunians thrawn ansuroer lobot cracken aleena lando saleucami ysanne. Panaka ackbar biggs kalee phlog moff kathol qel-droma. Ubese anx yoda atrivis jek chommell freedon k-3po ponda. Carondian axmis jabba fel dooku sunrider amidala dash. Ziro voxyn mara xanatos whitesun wat. Vader lannik yané ortolan durron sebulba. Moor kurtzen soontir abrion kael jabba nelvaanian luke wicket.</p>
+
+
+<?php
+include('templates/footer.html');
+?>
