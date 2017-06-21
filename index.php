bf2752589c9f88f4b853d5aced4ae202333096fe
jprinz <jprinz@conestogac.on.ca>
2017-06-21 09:13:54 -0400

    Changed filler text

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ define('TITLE', 'Raise High the Roof Beam!');
 include('templates/header.html');
 ?>
     <h2>Welcome to a J.D. Salinger Fan Club</h2>
-    <p>Lucas ipsum dolor sit amet motti malastare c-3p0 ti hypori skywalker ooryl sio noa iego. Coruscant hssis calamari nagai. Boltrunians thrawn ansuroer lobot cracken aleena lando saleucami ysanne. Panaka ackbar biggs kalee phlog moff kathol qel-droma. Ubese anx yoda atrivis jek chommell freedon k-3po ponda. Carondian axmis jabba fel dooku sunrider amidala dash. Ziro voxyn mara xanatos whitesun wat. Vader lannik yané ortolan durron sebulba. Moor kurtzen soontir abrion kael jabba nelvaanian luke wicket.</p>
+    <p>Walking boss has been the ruin of me o'er Jordan troublin' mind! Shady grove, five dollars black as coal cold wind blows baby on her knee the buggies and the hacks all formed in line mule skinner down to the river. What I had done. Five dollars on the run. Steel driving crew you've robbed my poor pocket Sally Ann! Black as coal. Steel driving crew.</p>
 
 
 <?php
